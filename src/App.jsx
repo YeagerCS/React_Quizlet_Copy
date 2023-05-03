47f811640e3a77f6d6277cc1f39895a270607ed9
Debug <lejsmalisi0@gmail.com>
2023-05-03 21:30:29 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import "./styles.css"
+
+export default function App(){
+  const [index, setIndex] = useState(() => {
+    if(localStorage.getItem("INDEX")){
+      return Number(localStorage.getItem("INDEX"))
+    }
+    
+
+    return 0
+  })
+  const [wordsEnglish, setWordsEnglish] = useState([])
+  const [wordsGerman, setWordsGerman] = useState([])
+  const [massage, setMassage] = useState("")
+  const [currentInput, setCurrentInput] = useState("")
+  const [answer, setAnswer] = useState(false)
+  const [inputEnglish, setInputEnglish] = useState(() =>{
+    if(localStorage.getItem("INPUT") !== null) {
+      return localStorage.getItem("INPUT") == "true" ? true : false
+    }
+    return true
+  })
+
+
+  function checkAnswer(e){
+    e.preventDefault()
+
+    if(currentInput.toLowerCase() == inputEnglish ? wordsEnglish[index].toLowerCase() : wordsGerman[index]){
+      setMassage("Correct")
+      setCurrentInput("")
+      setIndex(prevIndex => prevIndex + 1)
+    } else{
+      setMassage("Incorrect")
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem("INDEX", index)
+  }, [index])
+
+  useEffect(() => {
+    fetch("data/words.json").then(response => response.json())
+    .then(data =>{
+      setWordsEnglish(data.english)
+      setWordsGerman(data.german)
+    })
+  }, [])
+
+  function resetState(){
+    localStorage.removeItem("INDEX"); 
+    setIndex(0);
+    setMassage("")
+    setAnswer(false)
+  }
+
+  function changeInputLanguage(){
+    setInputEnglish(!inputEnglish)
+  }
+
+  useEffect(() =>{
+    localStorage.setItem("INPUT", inputEnglish)
+  }, [inputEnglish])
+  
+
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li><button onClick={resetState} className="btnStyle">Delete State</button><br/></li>
+            <li><button className="btnStyle" onClick={changeInputLanguage}>{inputEnglish ? "Set Input to German" : "Set Input to English"}</button></li>
+          </ul>
+        </nav>
+      </header>
+      <div id="maindiv">
+        <label htmlFor="words">{inputEnglish ? wordsGerman[index] : wordsEnglish[index]}</label> 
+        <input type="text" className="boxStyle" name="words" id="words" placeholder="Answer..." value={currentInput} onChange={e => setCurrentInput(e.target.value)}/>
+        <button className="btnStyle" onClick={checkAnswer}>Check</button><em><button className="btnStyle" onClick={() => setAnswer(!answer)}>{answer ? "Hide answer" : "Show answer"}</button></em> 
+        <p>{answer ? inputEnglish ? wordsEnglish[index] : wordsGerman[index] : ""}&#x200B;</p>
+        <p className={`message ${massage === "Correct" ? "correct" : "incorrect"}`}>{massage}&#x200B;</p>
+      </div>
+    </>
+  )
+  
+}
